d3da9937c391f6f891453a3c5aad1b39752c30f8
Mounir KHANOURI <mkhanouri555@gmail.com>
2021-03-29 23:08:21 +0100

    new mongoose module and client module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientModule } from './client/client.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1/crm-client'),
+    ClientModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
