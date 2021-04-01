b33086a7a3d4c9383086f985c88dd5da2035820d
Mounir KHANOURI <mkhanouri555@gmail.com>
2021-04-01 10:15:38 +0100

    defining client schema

---
+++ src/client/schemas/client.schema.ts
@@ -0,0 +1,33 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type ClientDocument = Client & Document;
+
+@Schema()
+export class Client {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop()
+  meetingDate: Date;
+
+  @Prop({ required: true })
+  contactDate: Date;
+
+  @Prop({ required: true })
+  negociationState: string;
+
+  @Prop()
+  rate: number;
+
+  @Prop([String])
+  skills: string[];
+
+  @Prop()
+  roleType: string;
+
+  @Prop({ required: true })
+  type: string;
+}
+
+export const ClientSchema = SchemaFactory.createForClass(Client);
