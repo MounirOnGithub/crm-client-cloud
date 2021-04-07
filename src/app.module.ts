d6d35be30862b81391653ffb862381c754fdf916
Mounir Khanouri <mounir.khanouri-ext@vestiairecollective.com>
2021-04-07 19:18:58 +0200

    created new module contact by nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientModule } from './client/client.module';
+import { ContactsModule } from './contacts/contacts.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1/crm-client'),
     ClientModule,
+    ContactsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
