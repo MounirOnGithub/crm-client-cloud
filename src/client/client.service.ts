7b66c9586bf6c253542901acfe4e9e4ffc059eba
Mounir Khanouri <mounir.khanouri-ext@vestiairecollective.com>
2021-04-07 14:17:12 +0200

    finding one client by id

--- src/client/client.service.ts
+++ src/client/client.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 
 import { Client, ClientDocument } from './schemas/client.schema';
 import { CreateClientDto } from './dto/create-client.dto';
@@ -18,8 +18,12 @@ export class ClientService {
     return createdClient.save();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} client`;
+  findOne(id: string): Promise<Client> {
+    return this.clientModel
+      .findById({
+        _id: id,
+      })
+      .exec();
   }
 
   update(id: number, updateClientDto: UpdateClientDto) {
